68d4e298bd9f1d124577b9706f02263872dafe6b
Radha <radhabhoyar07@gmail.com>
2026-03-29 00:11:05 +0530

    updated

--- dict.py
+++ dict.py
@@ -2,3 +2,13 @@ messages = [{"role":"user","content":"Hello"}, {"role":"assistant","content":"Hi
 texts = [m["content"] for m in messages] 
 print(texts)
 
+words = ['cast', 'elephant','ox']
+lengths = [len(w) for w in words]
+print(lengths)
+
+results = [x*2 for x in [2,6,92,81]]
+print(results)
+
+fruits = ['cherry','berries', 'mango','pineapple']
+value = [len(f) for f in [fruits]]
+print(value)
