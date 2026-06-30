bddb4c1c636369eb43438b807cc8ba7943ad7b01
Radha <radhabhoyar07@gmail.com>
2026-06-30 23:41:42 +0530

    basic pass

---
+++ fundamentals/pass.py
@@ -0,0 +1,13 @@
+'''class spidevice:
+    pass
+
+print('class spidevice has been defined')
+print(spidevice)'''
+
+
+class	SPIDevice:
+    """Represents	a	generic	SPI	device	on	the	bus."""
+    
+    
+print("Class	SPIDevice	has	been	defined.")
+print(SPIDevice.__doc__)
